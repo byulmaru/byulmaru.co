b4143f6620ef0e992377b70b60306a49ca0f23c7
robin* <me@jiyu.land>
2026-01-12 13:47:47 +0900

    사이드바 메뉴 정리

--- src/routes/(main)/AppSidebar.svelte
+++ src/routes/(main)/AppSidebar.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
-  import { KeyRoundIcon, MailIcon, UserIcon } from '@lucide/svelte';
+  import {
+    ChevronDownIcon,
+    MegaphoneIcon,
+    UserIcon,
+  } from '@lucide/svelte';
   import { resolve } from '$app/paths';
   import { page } from '$app/state';
   import * as Sidebar from '$lib/components/ui/sidebar';
   import NavAccount from './NavAccount.svelte';
+  import * as Collapsible from '$lib/components/ui/collapsible';
 
   type Props = {
-    account: {
-      name: string;
-      primaryEmail: string;
-    } | undefined;
+    account:
+      | {
+          name: string;
+          primaryEmail: string;
+        }
+      | undefined;
   };
 
   const { account }: Props = $props();
@@ -19,115 +26,89 @@
 <Sidebar.Root>
   <Sidebar.Header>
     <Sidebar.Menu>
-        <Sidebar.MenuItem>
-          <Sidebar.MenuButton>
-            {#snippet child({ props })}
-              <a href={resolve('/')} {...props}>
-                <span class="font-bold">별마루동인협동조합(가칭)</span>
-              </a>
-            {/snippet}
-          </Sidebar.MenuButton>
-        </Sidebar.MenuItem>
-      </Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton>
+          {#snippet child({ props })}
+            <a href={resolve('/')} {...props}>
+              <span class="font-bold">별마루동인협동조합(가칭)</span>
+            </a>
+          {/snippet}
+        </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
   </Sidebar.Header>
   <Sidebar.Content>
-    <Sidebar.Group>
-      <Sidebar.GroupLabel>계정 설정</Sidebar.GroupLabel>
-      <Sidebar.GroupContent>
-        <Sidebar.Menu>
+    <Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton>
+          {#snippet child({ props })}
+            <a href={resolve('/')} {...props}>
+              <MegaphoneIcon />
+              <span>공고</span>
+            </a>
+          {/snippet}
+        </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+    {#if account}
+      <Sidebar.Menu>
+        <Collapsible.Root open class="group/collapsible">
           <Sidebar.MenuItem>
-            <Sidebar.MenuButton isActive={isActive('/settings/profile')}>
+            <Collapsible.Trigger>
               {#snippet child({ props })}
-                <a href={resolve('/(main)/settings/profile')} {...props}>
+                <Sidebar.MenuButton {...props}>
                   <UserIcon />
-                  <span>프로필</span>
-                </a>
+                  <span class="flex-1">계정</span>
+                  <ChevronDownIcon
+                    class="ms-auto transition-transform group-data-[state=open]/collapsible:rotate-180"
+                  />
+                </Sidebar.MenuButton>
               {/snippet}
-            </Sidebar.MenuButton>
+            </Collapsible.Trigger>
+            <Collapsible.Content>
+              <Sidebar.MenuSub>
+                <Sidebar.MenuSubItem>
+                  <Sidebar.MenuSubButton>
+                    {#snippet child({ props })}
+                      <a href={resolve('/(main)/account')} {...props}>
+                        <span>일반</span>
+                      </a>
+                    {/snippet}
+                  </Sidebar.MenuSubButton>
+                </Sidebar.MenuSubItem>
+                <Sidebar.MenuSubItem>
+                  <Sidebar.MenuSubButton>
+                    {#snippet child({ props })}
+                      <a href={resolve('/(main)/account/email')} {...props}>
+                        <span>이메일</span>
+                      </a>
+                    {/snippet}
+                  </Sidebar.MenuSubButton>
+                </Sidebar.MenuSubItem>
+                <Sidebar.MenuSubItem>
+                  <Sidebar.MenuSubButton>
+                    {#snippet child({ props })}
+                      <a href={resolve('/(main)/account/passkey')} {...props}>
+                        <span>패스키</span>
+                      </a>
+                    {/snippet}
+                  </Sidebar.MenuSubButton>
+                </Sidebar.MenuSubItem>
+                <Sidebar.MenuSubItem>
+                  <Sidebar.MenuSubButton>
+                    {#snippet child({ props })}
+                      <a href={resolve('/(main)/account/discord')} {...props}>
+                        <span>디스코드</span>
+                      </a>
+                    {/snippet}
+                  </Sidebar.MenuSubButton>
+                </Sidebar.MenuSubItem>
+              </Sidebar.MenuSub>
+            </Collapsible.Content>
           </Sidebar.MenuItem>
-          <Sidebar.MenuItem>
-            <Sidebar.MenuButton isActive={isActive('/settings/email')}>
-              {#snippet child({ props })}
-                <a href={resolve('/(main)/settings/email')} {...props}>
-                  <MailIcon />
-                  <span>이메일</span>
-                </a>
-              {/snippet}
-            </Sidebar.MenuButton>
-          </Sidebar.MenuItem>
-        </Sidebar.Menu>
-      </Sidebar.GroupContent>
-    </Sidebar.Group>
-    <Sidebar.Group>
-      <Sidebar.GroupLabel>보안 설정</Sidebar.GroupLabel>
-      <Sidebar.GroupContent>
-        <Sidebar.Menu>
-          <!-- <Sidebar.MenuItem>
-            <Sidebar.MenuButton
-              isActive={isActive('/password')}
-            >
-              {#snippet child({ props })}
-              <a href="/password" {...props}>
-                <LockIcon/>
-                <span>비밀번호</span>
-              </a>
-              {/snippet}
-            </Sidebar.MenuButton>
-          </Sidebar.MenuItem> -->
-          <Sidebar.MenuItem>
-            <Sidebar.MenuButton isActive={isActive('/passkey')}>
-              {#snippet child({ props })}
-                <a href={resolve('/(main)/settings/passkey')} {...props}>
-                  <KeyRoundIcon />
-                  <span>패스키</span>
-                </a>
-              {/snippet}
-            </Sidebar.MenuButton>
-          </Sidebar.MenuItem>
-          <!-- <Sidebar.MenuItem>
-          <Sidebar.MenuButton
-            isActive={isActive('/passkey')}
-          >
-            {#snippet child({ props })}
-            <a href="/otp" {...props}>
-              <RectangleEllipsisIcon />
-              <span>OTP</span>
-            </a>
-            {/snippet}
-          </Sidebar.MenuButton>
-        </Sidebar.MenuItem>
-        <Sidebar.MenuItem>
-          <Sidebar.MenuButton
-            isActive={isActive('/sessions')}
-          >
-            {#snippet child({ props })}
-            <a href="/sessions" {...props}>
-              <ListIcon/>
-              <span>세션 목록</span>
-            </a>
-            {/snippet}
-          </Sidebar.MenuButton>
-        </Sidebar.MenuItem> -->
-        </Sidebar.Menu>
-      </Sidebar.GroupContent>
-    </Sidebar.Group>
-    <Sidebar.Group>
-      <Sidebar.GroupLabel>외부 계정 설정</Sidebar.GroupLabel>
-      <Sidebar.GroupContent>
-        <Sidebar.Menu>
-          <Sidebar.MenuItem>
-            <Sidebar.MenuButton isActive={isActive('/discord')}>
-              {#snippet child({ props })}
-                <a href={resolve('/(main)/settings/discord')} {...props}>
-                  <iconify-icon icon="fa7-brands:discord"></iconify-icon>
-                  <span>디스코드</span>
-                </a>
-              {/snippet}
-            </Sidebar.MenuButton>
-          </Sidebar.MenuItem>
-        </Sidebar.Menu>
-      </Sidebar.GroupContent>
-    </Sidebar.Group>
+        </Collapsible.Root>
+      </Sidebar.Menu>
+    {/if}
   </Sidebar.Content>
   <Sidebar.Footer>
     {#if account}
